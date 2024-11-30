3212a910b636b60e3443c182bcba501eaa55b53c
MalluruMamatha <mamatha.mm939@gmail.com>
2024-12-01 00:14:35 +0530

    hcl project

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,23 +173,9 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
-# IAM Role for ECS Task
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs-task-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect   = "Allow"
-        Sid      = ""
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
 }
 
 # ECS Service with Load Balancer
