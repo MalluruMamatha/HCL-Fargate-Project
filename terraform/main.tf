428e414d581b8725b3e754b562eec950def5e4c3
MalluruMamatha <mamatha.mm939@gmail.com>
2024-12-01 00:51:07 +0530

    hcl project

--- terraform/main.tf
+++ terraform/main.tf
@@ -153,6 +153,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_execution_role.name
 }
 
+
+
+
 # IAM Role for ECS Task (Create this missing IAM role)
 resource "aws_iam_role" "ecs_task_role" {
   name = "ecs-task-role"
@@ -171,6 +174,27 @@ resource "aws_iam_role" "ecs_task_role" {
     ]
   })
 }
+
+resource "aws_security_group" "ecs_task_security_group" {
+  name        = "ecs-task-sg"
+  description = "Allow ECS tasks to communicate with ECR"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 # ECS Task Definition with ECR Image
 resource "aws_ecs_task_definition" "appointment_task" {
